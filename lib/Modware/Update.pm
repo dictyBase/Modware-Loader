fda04c06566bdbec192998e1819cd6a7eeb6222a
Siddhartha Basu <biosidd@gmail.com>
2011-05-09 10:31:39 -0500

    Added base class for update modules

---
+++ lib/Modware/Update.pm
@@ -0,0 +1,21 @@
+package Modware::Update;
+
+
+# Other modules:
+use Moose;
+use namespace::autoclean;
+extends qw/MooseX::App::Cmd/;
+
+# Module implementation
+#
+
+
+1;    # Magic true value required at end of module
+
+__END__
+
+=head1 NAME
+
+<Modware::Update> - [Base class for all Update modules]
+
+
