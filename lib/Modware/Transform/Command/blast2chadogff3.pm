8a73712d111f29758cb7b4af73cf9afd5afc14d5
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-02-06 13:11:45 -0600

    The gff3 output assumes the query to be in the report. The subject or hit assumed to be the reference sequence. Added description parser

--- lib/Modware/Transform/Command/blast2chadogff3.pm
+++ lib/Modware/Transform/Command/blast2chadogff3.pm
@@ -23,7 +23,7 @@ has 'source' => (
     lazy        => 1,
     default     => sub {
         my ($self) = @_;
-        return $self->_result_object->algorithm;
+        return lc $self->_result_object->algorithm;
     },
     documentation =>
         'the source field of GFF output,  default will the algorithm name'
@@ -41,6 +41,9 @@ has 'primary_tag' => (
             $tag = 'nucleotide_match';
         }
         elsif ( $algorithm eq 'blastp' ) {
+            $tag = 'protein_match';
+        }
+        elsif ( $algorithm eq 'tblastn' ) {
             $tag = 'protein_match';
         }
         else {
@@ -83,17 +86,108 @@ has 'hit_counter' => (
     handles => { inc_hit => 'inc', }
 );
 
+has 'hit_id_parser' => (
+    is  => 'rw',
+    isa => 'Str',
+    documentation =>
+        'hit id parser for the header line,  default is to use none ncbi,regular parsers are available'
+);
+
+has 'query_id_parser' => (
+    is  => 'rw',
+    isa => 'Str',
+    documentation =>
+        'query id parser for the header line,  default is to use none. ncbi,regular parsers are available'
+);
+
+has 'desc_parser' => (
+    is  => 'rw',
+    isa => 'Str',
+    documentation =>
+        'description parser for the header line,  default is to use none. ncbi parser is available'
+);
+
+has '_desc_parser_stack' => (
+    is      => 'rw',
+    isa     => 'HashRef',
+    traits  => [qw/Hash/],
+    handles => {
+        get_desc_parser      => 'get',
+        register_desc_parser => 'set',
+    },
+    default => sub {
+        my ($self) = @_;
+        return { 'ncbi' => sub { $self->ncbi_desc_parser(@_) }, };
+    },
+    lazy => 1
+);
+
+has '_parser_stack' => (
+    is      => 'rw',
+    isa     => 'HashRef',
+    traits  => [qw/Hash/],
+    handles => {
+        get_parser      => 'get',
+        register_parser => 'set',
+    },
+    default => sub {
+        my ($self) = @_;
+        return {
+            'ncbi'    => sub { $self->ncbi_parser(@_) },
+            'regular' => sub { $self->regular_parser(@_) }
+        };
+    },
+    lazy => 1
+);
+
+sub ncbi_parser {
+    my ( $self, $string ) = @_;
+    return $string if $string !~ /\|/;
+    return ( ( split /\|/, $string ) )[1];
+}
+
+sub regular_parser {
+    my ( $self, $string ) = @_;
+    return $string if $string !~ /\|/;
+    return ( ( split /\|/, $string ) )[0];
+}
+
+sub ncbi_desc_parser {
+    my ( $self, $string ) = @_;
+    return $string if $string !~ /\|/;
+    my @values = ( split /\|/, $string ) ;
+    my $desc = $values[-1];
+    $desc =~ s/^\s*//g;
+    $desc =~ s/\s*$//g;
+    return $desc;
+}
+
 sub execute {
     my ($self) = @_;
     my $parser
         = Bio::SearchIO->new( -format => 'blast', -file => $self->input );
-    my $out = Bio::Tools::GFF->new( -file => ">".$self->output,  -gff_version => 3 );
-
+    my $out = Bio::Tools::GFF->new(
+        -file        => ">" . $self->output,
+        -gff_version => 3
+    );
 
 RESULT:
     while ( my $result = $parser->next_result ) {
-        my $qname = $result->query_name;
-        my $desc  = $result->query_description;
+
+        my $qname
+            = $self->query_id_parser
+            ? $self->get_parser( $self->query_id_parser )
+            ->( $result->query_name )
+            : $result->query_name;
+        my $qacc
+            = $result->query_accession ? $result->query_accession : $qname;
+
+        my $qdesc
+            = $self->desc_parser
+            ? $self->get_desc_parser( $self->desc_parser )
+            ->( $result->query_description )
+            : $result->query_description;
+
         $self->_result_object($result) if !$self->has_result_object;
     HIT:
         while ( my $hit = $result->next_hit ) {
@@ -101,59 +195,55 @@ RESULT:
                 if $self->has_cutoff
                     and ( $hit->significance > $self->cutoff );
 
-            my $name     = $hit->name;
-            my $acc      = $qname . '-' . $name;
-            my $gff_name = $name . $self->hit_counter;
-            my @hsps;
+            my $hname
+                = $self->hit_id_parser
+                ? $self->get_parser( $self->hit_id_parser )->( $hit->name )
+                : $hit->name;
+            my $hacc = $hit->accession ? $hit->accession : $hname;
+
             if ( $self->group ) {
-                @hsps = sort {$a->start('hit') <=> $b->start('hit') }$hit->hsps;
                 $out->write_feature(
                     Bio::SeqFeature::Generic->new(
-                        -start       => $hsps[0]->start('hit'),
-                        -end         => $hsps[-1]->end('hit'),
-                        -seq_id      => $name,
+                        -start       => $hit->start('hit'),
+                        -end         => $hit->end('hit'),
+                        -seq_id      => $hname,
                         -source_tag  => $self->source,
                         -primary_tag => $self->primary_tag,
-                        -tag         => { 'ID' => $gff_name }
+                        -score       => $hit->significance,
+                        -tag => { 'ID' => $qname, 'Description' => $qdesc }
                     )
                 );
-                $self->inc_hit;
             }
 
-            my $counter = 01;
-            for my $hsp (@hsps )
-                {
-                    my $feature = Bio::SeqFeature::Generic->new;
-                    $feature->seq_id($name);
-                    $feature->primary_tag('match_part');
-                    $feature->score( $hsp->evalue );
-                    $feature->start( $hsp->start('hit') );
-                    $feature->end( $hsp->end('hit') );
-                    $feature->strand( $hit->strand );
-                    $feature->source_tag( $self->source );
-                    $feature->add_tag_value( 'ID',   $acc . $counter );
-                    $feature->add_tag_value( 'Name', $desc );
+            while ( my $hsp = $hit->next_hsp ) {
+                my $feature = Bio::SeqFeature::Generic->new();
+                $feature->seq_id($hname);
+                $feature->primary_tag('match_part');
+                $feature->score( $hsp->significance );
+                $feature->start( $hsp->start('subject') );
+                $feature->end( $hsp->end('subject') );
+                $feature->strand( $hsp->strand('hit') );
+                $feature->source_tag( $self->source );
 
-                    if ( $self->group ) {
-                        $feature->add_tag_value( 'Parent', $gff_name );
-                    }
+                if ( $self->group ) {
+                    $feature->add_tag_value( 'Parent', $qname );
+                }
 
-                    if ( $self->add_target ) {
-                        $feature->add_tag_value( 'Target', $qname );
-                        $feature->add_tag_value( 'Target', $hsp->start );
-                        $feature->add_tag_value( 'Target', $hsp->end );
-                        $feature->add_tag_value( 'Target', $hsp->strand );
+                if ( $self->add_target ) {
+                    $feature->add_tag_value( 'Target', $qname );
+                    $feature->add_tag_value( 'Target', $hsp->start );
+                    $feature->add_tag_value( 'Target', $hsp->end );
+                    $feature->add_tag_value( 'Target', $hsp->strand );
 
-                        my @str = unpack "(A2)*", $hsp->cigar_string;
-                        $feature->add_tag_value( 'Gap', join( ' ', @str ) );
-                        $counter++;
-                    }
-                    $out->write_feature($feature);
+                    my @str = unpack "(A2)*", $hsp->cigar_string;
+                    $feature->add_tag_value( 'Gap', join( ' ', @str ) );
+                }
+
+                $out->write_feature($feature);
             }
         }
     }
 }
-
 __PACKAGE__->meta->make_immutable;
 
 1;    # Magic true value required at end of module
