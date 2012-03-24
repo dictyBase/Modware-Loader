c244c723081a7841c3e225f67a52eebb277c99f2
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-03-24 17:29:51 -0500

    Imports type from Modware::Load::Types

--- lib/Modware/Role/Command/WithInput.pm
+++ lib/Modware/Role/Command/WithInput.pm
@@ -5,22 +5,17 @@ use strict;
 # Other modules:
 use namespace::autoclean;
 use Moose::Role;
-use Moose::Util::TypeConstraints;
-use Path::Class::File;
 use IO::Handle;
+use Modware::Load::Types qw/FileObject/;
 
 # Module implementation
 #
-subtype 'FileObject' => as class_type('Path::Class::File');
-coerce 'FileObject' => from 'Str' => via { Path::Class::File->new($_) };
 
 has 'output' => (
     is          => 'rw',
     isa         => 'FileObject',
     traits      => [qw/Getopt/],
     cmd_aliases => 'o',
-
-    #required      => 1,
     coerce        => 1,
     predicate     => 'has_output',
     documentation => 'Name of the output file'
