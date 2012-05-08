ae2bdd89c6aa544569fec91d8dae064157bc52b6
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-05-08 15:21:01 -0500

    Added method for writing exons Added method for generating gff3 hash for pseudogenic gene models

--- lib/Modware/Export/Command/chado2dictygff3.pm
+++ lib/Modware/Export/Command/chado2dictygff3.pm
@@ -3,6 +3,7 @@ package Modware::Export::Command::chado2dictygff3;
 use strict;
 use namespace::autoclean;
 use Moose;
+use Bio::GFF3::LowLevel qw/gff3_format_feature/;
 
 extends qw/Modware::Export::Command::chado2gff3/;
 
@@ -65,7 +66,13 @@ sub write_transcript_feature {
 
         # dicty pseudogene gene model have to be SO complaint
         # it writes gene and transcript feature
-        $self->pseudogene2gff3( $dbrow, $seq_id, $gene_id, $output );
+        my $pseudogene_hash
+            = $self->pseudorow2gff3hash( $self->gene_row, $seq_id, $gene_id,
+            'pseudogene' );
+        my $trans_hash = $self->pseudorow2gff3hash( $dbrow, $seq_id, $gene_id,
+            'pseudogenic_transcript' );
+        $output->print( gff3_format_feature($pseudogene_hash) );
+        $output->print( gff3_format_feature($trans_hash) );
     }
     else {
 
@@ -84,13 +91,66 @@ sub write_exon_feature {
     my ( $self, $dbrow, $seq_id, $trans_id, $output ) = @_;
     my $rs = $self->schema->resultset('Sequence::Feature')
         ->search( { 'dbxref.accession' => $trans_id }, { join => 'dbxref' } );
+
+    my $hash;
     if ( $rs->first->type->name eq 'pseudogene' ) {
-        $self->pseudoexon2gff3( $dbrow, $seq_id, $trans_id, $output );
+        $hash = $self->pseudorow2gffhash( $dbrow, $seq_id, $trans_id,
+            'pseudogenic_exon' );
     }
     else {
-        my $hash = $self->_dbrow2gff3hash( $dbrow, $seq_id, $trans_id );
-        $output->print( gff3_format_feature($hash) );
+        $hash = $self->_dbrow2gff3hash( $dbrow, $seq_id, $trans_id );
     }
+    $output->print( gff3_format_feature($hash) );
+}
+
+sub pseudorow2gff3hash {
+    my ( $self, $dbrow, $seq_id, $parent_id, $type ) = @_;
+    my $hashref;
+    $hashref->{type}   = $type;
+    $hashref->{seq_id} = $seq_id;
+    $hashref->{score}  = undef;
+    $hashref->{phase}  = undef;
+
+    my $floc_row = $dbrow->featureloc_features->first;
+    $hashref->{start} = $floc_row->fmin + 1;
+    $hashref->{end}   = $floc_row->fmax;
+    if ( my $strand = $floc_row->strand ) {
+        $hashref->{strand} = $strand == -1 ? '-' : '+';
+    }
+    else {
+        $hashref->{strand} = undef;
+    }
+
+    # source
+    my $dbxref_rs
+        = $dbrow->search_related( 'feature_dbxrefs', {} )->search_related(
+        'dbxref',
+        { 'db.name' => 'GFF_source' },
+        { join      => 'db' }
+        );
+    if ( my $row = $dbxref_rs->first ) {
+        $hashref->{source} = $row->accession;
+    }
+    else {
+        $hashref->{source} = undef;
+    }
+
+    ## -- attributes
+    $hashref->{attributes}->{ID} = [ $self->_chado_feature_id($dbrow) ];
+    if ( my $name = $dbrow->name ) {
+        $hashref->{attributes}->{Name} = [$name];
+    }
+    $hashref->{attributes}->{Parent} = [$parent_id] if $parent_id;
+    my $dbxrefs;
+    for my $xref_row ( grep { $_->db->name ne 'GFF_source' }
+        $dbrow->secondary_dbxrefs )
+    {
+        my $dbname = $xref_row->db->name;
+        $dbname =~ s/^DB:// if $dbname =~ /^DB:/;
+        push @$dbxrefs, $dbname . ':' . $xref_row->accession;
+    }
+    $hashref->{attributes}->{Dbxref} = $dbxrefs if defined @$dbxrefs;
+    return $hashref;
 }
 
 1;    # Magic true value required at end of module
