6a1d14614db3b6a750212ec20d8c3e515d7ac2e3
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-08-15 15:41:05 -0500

    Refactor creation of every file into its on method

--- lib/Modware/Generator/Command/create.pm
+++ lib/Modware/Generator/Command/create.pm
@@ -11,31 +11,12 @@ extends qw/Modware::Generator::Command/;
 
 # Module implementation
 #
-sub execute {
-    my ( $self, $opt, $argv ) = @_;
-    my $logger = $self->output_logger;
+has [qw/group command/] => (is => 'rw',  isa => 'Str',  traits => [qw/NoGetopt/]);
 
-    # make sure proper arguments are given
-    $logger->error_die('Need both group[GROUP] and command[COMMAND] names')
-        if !@$argv;
-    $logger->error_die('Need command[COMMAND] name') if @$argv == 1;
-
-    my $group = camelize $argv->[0];
-    my $cmd   = $argv->[1];
-
-    my $basedir = Path::Class::Dir->new($Bin)->parent->subdir('lib')
-        ->subdir('Modware');
-    $logger->logdie("expected base directory $basedir do not exist")
-        if !-e $basedir;
-
-    my $cmddir = $basedir->subdir($group)->subdir('Command');
-    make_path $cmddir, { verbose => 1 };
-
-    my $group_file = $basedir->file( $group . '.pm' );
-    $logger->logdie("$group_file exists !!! cannot create a new one")
-        if -e $group_file;
-
+sub create_group_file {
+    my ( $self, $group_file ) = @_;
     my $group_handler = $group_file->openw;
+    my $group = $self->group;
     $group_handler->print("package Modware::$group;\n\n");
     $group_handler->print(<<'GROUP');
 
@@ -54,12 +35,12 @@ GROUP
 
     $group_handler->print("Modware::$group - [Base application class]\n");
     $group_handler->close;
-    $logger->info("Created $group_file");
+}
 
-    my $cmd_class_file = $basedir->subdir($group)->file('Command.pm');
-    $logger->logdie("$cmd_class_file exists !!! cannot create a new one")
-        if -e $cmd_class_file;
+sub create_cmd_class_file {
+    my ( $self, $cmd_class_file ) = @_;
     my $cmd_class_handler = $cmd_class_file->openw;
+    my $group = $self->group;
     $cmd_class_handler->print(
         "package Modware::" . $group . "::Command;\n\n" );
     $cmd_class_handler->print(<<'CMD_CLASS');
@@ -76,12 +57,13 @@ __PACKAGE__->meta->make_immutable;
 CMD_CLASS
 
     $cmd_class_handler->close;
-    $logger->info("Created $cmd_class_file");
+}
 
-    my $cmd_file = $cmddir->file( $cmd . '.pm' );
-    $logger->logdie("$cmd_file exists cannot create a new one")
-        if -e $cmd_file;
+sub create_cmd_file {
+    my ( $self, $cmd_file ) = @_;
     my $cmd_file_handler = $cmd_file->openw;
+    my $group = $self->group;
+    my $cmd = $self->command;
     $cmd_file_handler->print(
         "package Modware::" . $group . "::Command::$cmd;\n\n" );
     $cmd_file_handler->print("use namespace::autoclean;\nuse Moose;\n");
@@ -89,8 +71,64 @@ CMD_CLASS
         "extends qw/Modware::" . $group . "::Command/;\n\n" );
     $cmd_file_handler->print(<<'CMD');
 
-sub execute {
+ sub execute {
   	my ($self) = @_;
+ }
+CMD
+    $cmd_file_handler->close;
+}
+
+sub execute {
+    my ( $self, $opt, $argv ) = @_;
+    my $logger = $self->output_logger;
+
+    # make sure proper arguments are given
+    $logger->error_die('Need both group[GROUP] and command[COMMAND] names')
+        if !@$argv;
+    $logger->error_die('Need command[COMMAND] name') if @$argv == 1;
+
+    my $group = camelize $argv->[0];
+    my $cmd   = $argv->[1];
+    $self->group($group);
+    $self->command($cmd);
+
+    my $basedir = Path::Class::Dir->new($Bin)->parent->subdir('lib')
+        ->subdir('Modware');
+    $logger->logdie("expected base directory $basedir do not exist")
+        if !-e $basedir;
+
+    my $cmddir = $basedir->subdir($group)->subdir('Command');
+    make_path $cmddir, { verbose => 1 };
+
+    # Command group file
+    my $group_file = $basedir->file( $group . '.pm' );
+    if ( -e $group_file ) {
+        $logger->warn("$group_file exists !!! skipping");
+    }
+    else {
+        $self->create_group_file($group_file);
+        $logger->info("Created $group_file");
+    }
+
+	# Command class file
+    my $cmd_class_file = $basedir->subdir($group)->file('Command.pm');
+    if ( -e $cmd_class_file ) {
+        $logger->warn("$cmd_class_file exists !!! skipping");
+    }
+    else {
+        $self->create_cmd_class_file($cmd_class_file);
+        $logger->info("Created $cmd_class_file");
+    }
+
+	# Command file
+    my $cmd_file = $cmddir->file( $cmd . '.pm' );
+    if ( -e $cmd_file ) {
+        $logger->logdie("$cmd_file exists!! skipping");
+    }
+    else {
+        $self->create_cmd_file($cmd_file);
+        $logger->info("Created $cmd_file");
+    }
 }
 
 1;
