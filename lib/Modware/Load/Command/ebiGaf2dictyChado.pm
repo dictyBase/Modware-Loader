02d68c4236f67a0f194fd52791d6df5c374a0cbc
Yogesh Pandit <yogeshp08@gmail.com>
2012-10-26 16:30:29 -0500

    Added extends with to make it run as a command

--- lib/Modware/Load/Command/ebiGaf2dictyChado.pm
+++ lib/Modware/Load/Command/ebiGaf2dictyChado.pm
@@ -7,7 +7,29 @@ use Bio::Chado::Schema;
 use Moose;
 use namespace::autoclean;
 
+extends qw/Modware::Load::Chado/;
+
 sub execute {
-
-
+    my ($self) = @_;
+	
 }
+
+1;
+
+=head1 NAME
+
+Modware::Load::Command::ebiGaf2dictyChado - Update dicty Chado with GAF from EBI
+
+=head1 SYNOPSIS
+ 
+=head1 REQUIRED ARGUMENTS
+
+=head1 OPTIONS
+
+=head1 DESCRIPTION
+
+Prune all the existing annotations from dicty Chado. Query EBI using the web-service for annotations for each Gene ID.
+Check if the link exists between feature and annotation; if yes, populate the retrieved data.
+
+=over
+
