96a500b91c5311cc30afd8d4294f762e52df296e
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-11-19 15:55:16 -0600

    misc changes

--- lib/Modware/Load/Command/bioportalobo2chado.pm
+++ lib/Modware/Load/Command/bioportalobo2chado.pm
@@ -29,72 +29,78 @@ sub execute {
     my $onto      = $bioportal->get_ontology( $self->ontology );
     my $itr       = $onto->get_all_terms;
 
-    my $total     = $itr->total_page;
-    my $intervals = $self->make_intervals($total, 10);
-    say Dumper $intervals;
-    exit;
+    my $total = $itr->total_page;
+    $self->logger->info("total page $total");
+    my $intervals = $self->make_intervals( $total, 10 );
 
     my $loop = IO::Async::Loop->new;
     my $func = IO::Async::Function->new(
         code => sub {
             $self->fetch_terms(@_);
-        }, 
-        min_workers => 3, 
+        },
+        min_workers => 3,
         max_workers => 5
     );
     $loop->add($func);
 
     my $counter = 0;
     for my $i ( 0 .. 2 ) {
-        my $slice = $itr->slice( @{ $intervals[$i] } );
+        my $slice = $itr->slice( @{ $intervals->[$i] } );
         $func->call(
             args      => [$slice],
-            on_return => sub { 
-              $self->load_terms(@_); 
-              if ($counter == 2) {
-              	$loop->stop;
-              }
-              $counter++;
+            on_return => sub {
+                $self->load_terms(@_);
+                if ( $counter == 2 ) {
+                    $loop->stop;
+                }
+                $counter++;
             },
-            on_error  => sub { $self->throw_error(@_) }
+            on_error => sub { $self->throw_error(@_) }
         );
     }
+    $loop->run;
 }
 
 sub fetch_terms {
-	my ($self, $itr) = @_;
-	my $all_terms;
-	while(my $term = $itr->next_term) {
-		push @$all_terms, $term;
-	}
-	return $all_terms;
+    my ( $self, $itr ) = @_;
+    my $all_terms;
+    while ( my $term = $itr->next_term ) {
+        push @$all_terms, $term;
+    }
+    return $all_terms;
 }
 
 sub load_terms {
-	my ($self, $all_terms) = @_;
-	say $_->name for @$all_terms;
+    my ( $self, $all_terms ) = @_;
+    $self->logger->info( $_->name ) for @$all_terms;
 }
 
 sub throw_error {
-	my ($self, $error) = @_;
-	warn "could not get terms $error\n";
+    my ( $self, $error ) = @_;
+    $self->logger->error("could not get terms $error");
 }
 
 sub make_intervals {
-	my ($self, $end, $increment) = @_;
-	my $slices;
-	my $start = 1;
-	while(1) {
-		my $next = $start + $increment;
-		if ($next >= $end) {
-			push @$slices, [$start, $end];
-			last;
-		}
-		push @$slices, [$start, $next];
-		$start = $next + 1;
-	}
-	return $slices;
+    my ( $self, $end, $increment ) = @_;
+    my $slices;
+    my $start = 1;
+    while (1) {
+        my $next = $start + $increment;
+        if ( $next >= $end ) {
+            push @$slices, [ $start, $end ];
+            last;
+        }
+        push @$slices, [ $start, $next ];
+        $start = $next + 1;
+    }
+    return $slices;
 }
+
+sub create_shared_data {
+	my ($self, $ontology) = @_;
+}
+
+1;
 
 __END__
 
