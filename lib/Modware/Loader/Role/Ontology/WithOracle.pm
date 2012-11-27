25454338ad1da3f44eba0c10efbf50902823ab4f
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-11-27 16:51:25 -0600

    BCS transformation for oracle primary to avoid the blob/clob fiasco

--- lib/Modware/Loader/Role/Ontology/WithOracle.pm
+++ lib/Modware/Loader/Role/Ontology/WithOracle.pm
@@ -30,9 +30,10 @@ sub handle_synonyms {
     );
 }
 
-sub setup {
-    my $self       = shift;
-    my $source     = $self->chado->source('Cv::Cvtermsynonym');
+sub transform_schema {
+    my ($self) = @_;
+    my $schema = $self->schema;
+    my $source = $self->schema->source('Cv::Cvtermsynonym');
     $source->remove_column('synonym');
     $source->add_column(
         'synonym_' => {
@@ -41,7 +42,22 @@ sub setup {
             size        => 1024
         }
     );
-
+    my @sources = (
+        'Cv::Cvprop',     'Cv::Cvtermprop',
+        'Cv::Dbxrefprop', 'Sequence::Featureprop',
+        'Sequence::FeatureCvtermprop'
+    );
+    for my $name (@sources) {
+        my $result_source = $schema->source($name);
+        next if !$result_source->has_column('value');
+        $result_source->remove_column('value');
+        $result_source->add_column(
+            'value' => {
+                data_type   => 'clob',
+                is_nullable => 1
+            }
+        );
+    }
 }
 
 1;    # Magic true value required at end of module
