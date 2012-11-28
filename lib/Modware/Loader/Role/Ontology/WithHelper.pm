5f0cd0739179fbff7fc21c4dd753ea157d991d00
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-11-28 16:14:36 -0600

    added find_or_create cvterm namespace method

--- lib/Modware/Loader/Role/Ontology/WithHelper.pm
+++ lib/Modware/Loader/Role/Ontology/WithHelper.pm
@@ -17,7 +17,6 @@ has '_cvrow' => (
     }
 );
 
-
 has '_cvterm_row' => (
     is        => 'rw',
     isa       => 'HashRef',
@@ -30,7 +29,6 @@ has '_cvterm_row' => (
         exist_cvterm_row => 'defined'
     }
 );
-
 
 has '_dbrow' => (
     is      => 'rw',
@@ -45,4 +43,51 @@ has '_dbrow' => (
     }
 );
 
+sub find_or_create_dbrow {
+    my ( $self, $db ) = @_;
+    my $schema = $self->schema;
+    my $dbrow  = $schema->resultset('General::Db')
+        ->find_or_create( { name => $db } );
+    $self->set_dbrow( $db, $dbrow ) if !$self->has_dbrow($db);
+    return $dbrow;
+}
+
+sub find_or_create_cvrow {
+    my ( $self, $cv ) = @_;
+    my $schema = $self->schema;
+    my $cvrow
+        = $schema->resultset('Cv::Cv')->find_or_create( { name => $cv } );
+    $self->set_cvrow( $cv, $cvrow )
+        if !$self->has_cvrow($cv);
+    return $cvrow;
+}
+
+sub find_or_create_cvterm_namespace {
+    my ($self, $cvterm, $cv, $db) = @_;
+    $cv  ||= 'cvterm_property_type';
+    $db  ||= 'internal';
+    my $schema = $self->schema;
+
+    my $cvterm_row
+        = $schema->resultset('Cv::Cvterm')->find( { name => $cvterm } );
+    if ($cvterm_row) {
+        $self->set_cvterm_row( $cvterm, $cvterm_row )
+            if !$self->has_cvterm_row($cvterm);
+    }
+    else {
+        my $dbxref_row
+            = $schema->resultset('General::Dbxref')->find_or_create(
+            {   accession => $cvterm, 
+                db_id     => $self->get_dbrow($db)->db_id
+            }
+            );
+        $cvterm_row = $schema->resultset('Cv::Cvterm')->create(
+            {   name      => $cvterm, 
+                cv_id     => $self->get_cvrow($cv)->cv_id,
+                dbxref_id => $dbxref_row->dbxref_id
+            }
+        );
+        $self->set_cvterm_row($cvterm, $cvterm_row);
+    }
+}
 1;
