57e92a774046724fff8d5691e210c1b5e68a27ff
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-11-28 13:21:37 -0600

    Added check for ontology version

--- lib/Modware/Load/Command/bioportalobo2chado.pm
+++ lib/Modware/Load/Command/bioportalobo2chado.pm
@@ -26,18 +26,27 @@ sub execute {
     my $logger = $self->logger;
 
     my $bioportal = BioPortal::WebService->new( apikey => $self->apikey );
-	my $downloader = $bioportal->downlaod($self->ontology);
-	if (!$downloader->is_obo_format) {
-		$logger->logcroak($self->ontology,  ' is not available in OBO format');
-	}
+    my $downloader = $bioportal->downlaod( $self->ontology );
+    if ( !$downloader->is_obo_format ) {
+        $logger->logcroak( $self->ontology,
+            ' is not available in OBO format' );
+    }
 
-	my $loader = Modware::Loader::Ontology->new;
-	my $ontology = OBO::Parser::OBOParser->new->work($downloader->filename);
-	$loader->set_ontology($ontology);
-	$loader->set_schema($self->schema);
+    my $loader   = Modware::Loader::Ontology->new;
+    my $ontology = OBO::Parser::OBOParser->new->work( $downloader->filename );
+    $loader->set_ontology($ontology);
+    $loader->set_schema( $self->schema );
+
+    # check if it is a new version
+    if ( $loader->is_ontology_in_db() ) {
+        if ( !$loader->is_ontology_new_version() ) {
+            $logger->logcroak(
+                "This version of ontology already exist in database");
+        }
+    }
+    $loader->store_metadata;
 
 }
-
 
 1;
 
