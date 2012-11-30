3467e925dc6c6614a5d92ef16418a194219b5616
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-11-30 14:21:19 -0600

    DBIC result classes for temporary tables

---
+++ Modware/Loader/Schema/Temporary.pm
@@ -0,0 +1,24 @@
+package Modware::Loader::Schema::Temporary;
+
+
+package Modware::Loader::Schema::Temporary::Cvterm';
+use strict;
+use base qw/DBIx::Class::Core/;
+
+__PACKAGE__->table('temp_cvterm');
+__PACKAGE__->add_columns(
+    'name' => { data_type => 'varchar', size => 1024 } );
+__PACKAGE__->add_columns(
+    'accession' => { data_type => 'varchar', size => 1024 } );
+__PACKAGE__->add_columns( 'definition' => { data_type => 'text' } );
+__PACKAGE__->add_columns( 'cmmt' => { data_type => 'text' } );
+__PACKAGE__->add_columns(
+    'is_relationshiptype' => { data_type => 'int', default => 0 } );
+__PACKAGE__->add_columns(
+    'is_obsolete' => { data_type => 'int', default => 0 } );
+
+
+1;
+
+1;    # Magic true value required at end of module
+
