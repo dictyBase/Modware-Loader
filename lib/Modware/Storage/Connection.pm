be23aaed80dfc4ddf129dacf1ac94695ee7997b1
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-12-03 14:45:01 -0600

    added schema_debug attribute

--- lib/Modware/Storage/Connection.pm
+++ lib/Modware/Storage/Connection.pm
@@ -6,6 +6,7 @@ has 'dsn' => (is => 'rw',  isa => 'Str');
 has [qw/user password/] => (is => 'rw',  isa => 'Str|Undef');
 has 'attribute' => (is => 'rw',  isa => 'HashRef');
 has 'extra_attribute' => (is => 'rw',  isa => 'HashRef');
+has 'schema_debug' => (is => 'rw',  isa => 'Bool',  default => 0,  lazy => 1);
 
 __PACKAGE__->meta->make_immutable;
 
