83fd3d62c5d93e0bd843779e46c4c5fb18d4a4be
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-12-03 14:39:51 -0600

    temp create statements run the sql statements

--- lib/Modware/Loader/Role/Ontology/WithSqlite.pm
+++ lib/Modware/Loader/Role/Ontology/WithSqlite.pm
@@ -34,7 +34,8 @@ sub handle_synonyms {
 sub transform_schema { }
 
 sub create_temp_statements {
-    return [
+    my ( $self, $storage ) = @_;
+    $storage->dbh->do(
         qq{
 	        CREATE TEMPORARY TABLE temp_cvterm (
                name varchar(1024) NOT NULL, 
@@ -42,13 +43,16 @@ sub create_temp_statements {
                is_obsolete integer NOT NULL DEFAULT 0, 
                is_relationshiptype integer NOT NULL DEFAULT 0, 
                definition text, 
-            )
-   	    }
-    ];
+               comment text, 
+               cv_id integer NOT NULL, 
+               db_id integer NOT NULL
+      }
+    );
 }
 
 sub drop_temp_statements {
-	return [ qq{ DROP TABLE temp_cvterm }];
+    my ( $self, $storage ) = @_;
+    $storage->dbh->do(qq{DROP TABLE temp_cvterm });
 }
 
 1;    # Magic true value required at end of module
