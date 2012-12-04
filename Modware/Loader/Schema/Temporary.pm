323a26565f676e9b6f4ccd12ec6699c81e255db7
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-12-04 13:47:16 -0600

    staging relationship table result class

--- Modware/Loader/Schema/Temporary.pm
+++ Modware/Loader/Schema/Temporary.pm
@@ -1,7 +1,7 @@
 package Modware::Loader::Schema::Temporary;
 
 
-package Modware::Loader::Schema::Temporary::Cvterm';
+package Modware::Loader::Schema::Temporary::Cvterm;
 use strict;
 use base qw/DBIx::Class::Core/;
 
@@ -20,6 +20,22 @@ __PACKAGE__->add_columns(
     'cv_id' => { data_type => 'int', nullable => 0 } );
 __PACKAGE__->add_columns(
     'db_id' => { data_type => 'int', nullable => 0 } );
+
+1;
+
+
+package Modware::Loader::Schema::Temporary::CvtermRelationship;
+use strict;
+use base qw/DBIx::Class::Core/;
+
+
+__PACKAGE__->table('temp_cvterm_relationship');
+__PACKAGE__->add_columns(
+    'subject' => { data_type => 'varchar', size => 1024 } );
+__PACKAGE__->add_columns(
+    'object' => { data_type => 'varchar', size => 1024 } );
+__PACKAGE__->add_columns(
+    'type' => { data_type => 'varchar', size => 256 } );
 
 1;
 
