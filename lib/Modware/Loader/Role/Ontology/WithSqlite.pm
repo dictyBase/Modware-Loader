2d1cda65ca2da63a910d812c1cbb8799aea35bca
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-12-11 10:44:33 -0600

    method for removing deleted terms during update left join of db_id between staging and dbxref table for creating dbxrefs. It allows to check for an unique id using accession and namespace(db_id) Identical db_id constraint added for creating cvterms,  relations and updating cvterms. Added create_cvterms_debug method where each term is inserted iteratively instead of one bulk insert statement. It is useful for debugging sql statements with errors.

--- lib/Modware/Loader/Role/Ontology/WithSqlite.pm
+++ lib/Modware/Loader/Role/Ontology/WithSqlite.pm
@@ -17,7 +17,7 @@ sub create_temp_statements {
     my ( $self, $storage ) = @_;
     $storage->dbh->do(
         qq{
-	        CREATE TEMPORARY TABLE temp_cvterm (
+	        CREATE TABLE IF NOT EXISTS temp_cvterm (
                name varchar(1024) NOT NULL, 
                accession varchar(1024) NOT NULL, 
                is_obsolete integer NOT NULL DEFAULT 0, 
@@ -30,21 +30,54 @@ sub create_temp_statements {
     );
     $storage->dbh->do(
         qq{
-	        CREATE TEMPORARY TABLE temp_accession (
+	        CREATE TABLE IF NOT EXISTS temp_accession (
                accession varchar(256) NOT NULL 
     )}
     );
     $storage->dbh->do(
         qq{
-	        CREATE TEMPORARY TABLE temp_cvterm_relationship (
-               subject varchar(1024) NOT NULL, 
-               object varchar(1024) NOT NULL, 
-               type varchar(1024) NULL 
+	        CREATE TABLE IF NOT EXISTS temp_cvterm_relationship (
+               subject varchar(256) NOT NULL, 
+               object varchar(256) NOT NULL, 
+               type varchar(256) NULL, 
+               subject_db_id integer NOT NULL, 
+               object_db_id integer NOT NULL, 
+               type_db_id integer NOT NULL
     )}
     );
 }
 
 sub drop_temp_statements {
+    my ( $self, $storage ) = @_;
+    $storage->dbh->do(qq{DELETE FROM temp_cvterm});
+    $storage->dbh->do(qq{DELETE FROM temp_accession});
+    $storage->dbh->do(qq{DELETE FROM temp_cvterm_relationship});
+}
+
+sub delete_non_existing_terms {
+	my ($self, $storage, $dbh) = @_;
+	my $data = $dbh->selectall_arrayref(
+		q{
+			SELECT cvterm.cvterm_id, dbxref.dbxref_id FROM cvterm
+			INNER JOIN dbxref ON cvterm.dbxref_id=dbxref.dbxref_id
+			LEFT JOIN temp_cvterm tmcv ON (
+				tmcv.accession = dbxref.accession
+				AND
+				tmcv.db_id = dbxref.db_id
+			)
+			WHERE tmcv.accession IS NULL
+			AND tmcv.db_id IS NULL
+			AND cvterm.cv_id IN (SELECT cv_id FROM temp_cvterm)
+			AND dbxref.db_id IN (SELECT db_id FROM temp_cvterm)
+		},  {Slice => {}}
+	);
+	
+	my $schema = $self->schema;
+	for my $row(@$data) {
+		$schema->resultset('Cv::Cvterm')->find($row->{cvterm_id})->delete;
+		$schema->resultset('General::Dbxref')->find($row->{dbxref_id})->delete;
+	}
+	return scalar @$data;
 }
 
 sub create_dbxrefs {
@@ -53,18 +86,28 @@ sub create_dbxrefs {
         q{
     		INSERT INTO temp_accession(accession)
 			SELECT tmcv.accession FROM temp_cvterm tmcv
-			LEFT JOIN dbxref ON tmcv.accession=dbxref.accession
-			INNER JOIN db ON db.db_id = tmcv.db_id
+			LEFT JOIN dbxref ON (
+			     tmcv.accession = dbxref.accession
+			     AND
+			     tmcv.db_id = dbxref.db_id
+            )
 			WHERE dbxref.accession is NULL
+			AND 
+			dbxref.db_id IS NULL
 			}
     );
     my $rows = $dbh->do(
         q{
 			INSERT INTO dbxref(accession, db_id)
 			SELECT tmcv.accession, tmcv.db_id FROM temp_cvterm tmcv
-			LEFT JOIN dbxref ON tmcv.accession=dbxref.accession
-			INNER JOIN db ON db.db_id = tmcv.db_id
+			LEFT JOIN dbxref ON (
+			     tmcv.accession = dbxref.accession
+			     AND
+			     tmcv.db_id = dbxref.db_id
+            )
 			WHERE dbxref.accession is NULL
+			AND 
+			dbxref.db_id IS NULL
 			}
     );
     return $rows;
@@ -78,17 +121,42 @@ sub create_cvterms {
     		  definition, cv_id, dbxref_id)
 			SELECT tmcv.name,tmcv.is_obsolete,tmcv.is_relationshiptype, 
 			tmcv.definition,tmcv.cv_id,dbxref.dbxref_id 
-			FROM temp_cvterm tmcv,temp_accession tmacc, dbxref,cv
-			WHERE tmcv.accession=tmacc.accession
-			AND dbxref.accession=tmcv.accession
-			AND dbxref.db_id=tmcv.db_id
+			FROM temp_cvterm tmcv
+			INNER JOIN temp_accession tmacc ON 
+			     tmcv.accession=tmacc.accession
+			INNER JOIN dbxref ON (
+			  dbxref.accession=tmcv.accession
+			  AND dbxref.db_id=tmcv.db_id
+			)
 			}
     );
     return $rows;
 }
 
+sub create_cvterms_debug {
+    my ( $self, $storage, $dbh ) = @_;
+    my $data = $dbh->selectall_arrayref(
+        q{
+			SELECT tmcv.name,tmcv.is_obsolete,tmcv.is_relationshiptype, 
+			tmcv.definition,tmcv.cv_id,dbxref.dbxref_id 
+			FROM temp_cvterm tmcv
+			INNER JOIN temp_accession tmacc ON 
+			     tmcv.accession=tmacc.accession
+			INNER JOIN dbxref ON (
+			  dbxref.accession=tmcv.accession
+			  AND dbxref.db_id=tmcv.db_id
+			)
+			}, { Slice => {} }
+    );
+    for my $row (@$data) {
+        $self->schema->resultset('Cv::Cvterm')->create($row);
+    }
+    return scalar @$data;
+}
+
 sub update_cvterm_names {
     my ( $self, $storage, $dbh ) = @_;
+
 #SQLite do not support JOINS in update statements,  so it's need to be done in few
 #more steps
 #This will update the name of cvterms.
@@ -96,10 +164,13 @@ sub update_cvterm_names {
         q{
     	SELECT fresh.* FROM (
     	   SELECT tmcv.name fname, cvterm.name oname, cvterm.cvterm_id
-    	   FROM temp_cvterm tmcv, dbxref, cvterm
-    	   WHERE dbxref.accession = tmcv.accession
-    	   AND dbxref.db_id=tmcv.db_id
-    	   AND cvterm.dbxref_id=dbxref.dbxref_id
+    		 FROM cvterm
+    		 INNER JOIN dbxref ON dbxref.dbxref_id = cvterm.dbxref_id
+    		 INNER JOIN temp_cvterm tmcv ON (
+    		 	dbxref.accession = tmcv.accession
+    		 	AND 
+    		 	dbxref.db_id = tmcv.db_id
+    		 )
     	) AS fresh
     	WHERE fresh.fname != fresh.oname
     }, { Slice => {} }
@@ -117,18 +188,18 @@ sub update_cvterm_names {
 
 sub update_cvterms {
     my ( $self, $storage, $dbh ) = @_;
-# This will update definition and status of all cvterms, as usual it is more work in case of SQLit existing cvterms
+
+# This will update definition and status of all cvterms, as usual it is more work in case
+# of SQLite existing cvterms
     my $data = $dbh->selectall_arrayref(
         q{
-    		SELECT cvterm.cvterm_id, tmcv.definition, tmcv.is_obsolete 
-    		 FROM cvterm, dbxref, temp_cvterm tmcv
-    		 WHERE  dbxref.dbxref_id=cvterm.dbxref_id
-    		 AND  tmcv.accession = dbxref.accession
-    		 AND  tmcv.db_id = dbxref.db_id
-    		 WHERE 
-    		  NOT EXISTS (
-    		    SELECT temp_accession.accession 
-    		    FROM temp_accession
+    		SELECT cvterm.cvterm_id, cvterm.name, tmcv.definition, tmcv.is_obsolete 
+    		 FROM cvterm
+    		 INNER JOIN dbxref ON dbxref.dbxref_id = cvterm.dbxref_id
+    		 INNER JOIN temp_cvterm tmcv ON (
+    		 	dbxref.accession = tmcv.accession
+    		 	AND 
+    		 	dbxref.db_id = tmcv.db_id
     		 )
     	}, { Slice => {} }
     );
@@ -151,18 +222,34 @@ sub create_relations {
     my ( $self, $storage, $dbh ) = @_;
     my $rows = $dbh->do(
         q{
-    	INSERT INTO cvterm_relationship(object_id, subject_id, type_id)
-    	SELECT object.cvterm_id, subject.cvterm_id, type.cvterm_id
-    	FROM temp_cvterm_relationship tmprel
-    	INNER JOIN cvterm object ON
-    	  tmprel.object = object.name
-		INNER JOIN cvterm subject ON
-		  tmprel.subject = subject.name
-		INNER JOIN cvterm type ON
-		  tmprel.type = type.name
-		 EXCEPT
-    	SELECT cvrel.object_id, cvrel.subject_id, cvrel.type_id
-    	FROM cvterm_relationship cvrel
+        INSERT INTO cvterm_relationship(object_id, subject_id, type_id)
+		SELECT object.cvterm_id, subject.cvterm_id, type.cvterm_id
+        FROM temp_cvterm_relationship tmprel
+
+        INNER JOIN dbxref dbobject ON (
+        	dbobject.accession = tmprel.object AND
+        	dbobject.db_id = tmprel.object_db_id 
+        )
+        INNER JOIN cvterm object ON
+        object.dbxref_id = dbobject.dbxref_id
+
+        INNER JOIN dbxref dbsubject ON (
+        	dbsubject.accession = tmprel.subject AND
+        	dbsubject.db_id = tmprel.subject_db_id 
+        )
+        INNER JOIN cvterm subject ON
+        subject.dbxref_id = dbsubject.dbxref_id
+
+        INNER JOIN dbxref dbtype ON (
+        	dbtype.accession = tmprel.type AND
+        	dbtype.db_id = tmprel.type_db_id 
+        )
+        INNER JOIN cvterm type ON
+        type.dbxref_id = dbtype.dbxref_id
+             
+      EXCEPT
+      SELECT cvrel.object_id, cvrel.subject_id, cvrel.type_id
+      FROM cvterm_relationship cvrel
     }
     );
     return $rows;
