dd55bab63061cc7aedd24df5fe8fdfa5f12fac89
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2012-12-13 15:51:38 -0600

    Change the column datatype for BCS result classes

--- lib/Modware/Loader/Role/Ontology/Chado/Oracle.pm
+++ lib/Modware/Loader/Role/Ontology/Chado/Oracle.pm
@@ -11,7 +11,36 @@ use feature qw/say/;
 has cache_threshold =>
     ( is => 'rw', isa => 'Int', lazy => 1, default => 5000 );
 
-sub transform_schema { }
+
+sub transform_schema {
+    my ($self) = @_;
+    my $schema = $self->schema;
+    my $source = $self->schema->source('Cv::Cvtermsynonym');
+    $source->remove_column('synonym');
+    $source->add_column(
+        'synonym_' => {
+            data_type   => 'varchar',
+            is_nullable => 0,
+            size        => 1024
+        }
+    );
+    my @sources = (
+        'Cv::Cvprop',     'Cv::Cvtermprop',
+        'Cv::Dbxrefprop', 'Sequence::Featureprop',
+        'Sequence::FeatureCvtermprop'
+    );
+    for my $name (@sources) {
+        my $result_source = $schema->source($name);
+        next if !$result_source->has_column('value');
+        $result_source->remove_column('value');
+        $result_source->add_column(
+            'value' => {
+                data_type   => 'clob',
+                is_nullable => 1
+            }
+        );
+    }
+}
 
 sub after_loading_in_staging {
     my ( $self, $storage, $dbh ) = @_;
