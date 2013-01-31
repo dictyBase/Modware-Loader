2bbd4d791c1f04e59787c5c31d26a7ac3c3c40cd
Yogesh Pandit <yogesh.pandit@northwestern.edu>
2013-01-31 15:13:20 -0600

    Caching evidence codes & references

--- lib/Modware/Loader/Role/GAF/DbxrefId.pm
+++ lib/Modware/Loader/Role/GAF/DbxrefId.pm
@@ -137,5 +137,72 @@ sub find_cvterm_id {
     }
 }
 
+has 'ev_codes' => (
+    is      => 'rw',
+    isa     => 'HashRef',
+    traits  => [qw/Hash/],
+    default => sub { {} },
+    handles => {
+        set_cvterm_id_for_ev => 'set',
+        get_cvterm_id_for_ev => 'get',
+        has_cvterm_for_ev    => 'defined'
+    }
+);
+
+sub find_cvterm_id_for_evidence_code {
+    my ( $self, $ev ) = @_;
+    if ( $self->has_cvterm_for_ev($ev) ) {
+        return $self->get_cvterm_id_for_ev($ev);
+    }
+    my $row
+        = $self->schema->resultset('Cv::Cv')
+        ->search( { 'name' => { -like => 'evidence_code%' } } )
+        ->first->cvterms->search_related(
+        'cvtermsynonyms',
+        {   'type.name' => { -in => [qw/EXACT RELATED BROAD/] },
+            'cv.name'   => 'synonym_type',
+            'synonym_'  => $ev
+        },
+        {   join   => { type => 'cv' },
+            cache  => 1,
+            select => 'cvterm_id'
+        }
+        );
+    if ( $row->count > 0 ) {
+        $self->set_cvterm_id_for_ev( $ev, $row->first->cvterm_id );
+        return $row->first->cvterm_id;
+    }
+}
+
+has 'publications' => (
+    is      => 'rw',
+    isa     => 'HashRef',
+    traits  => [qw/Hash/],
+    default => sub { {} },
+    handles => {
+        set_pub_id => 'set',
+        get_pub_id => 'get',
+        has_pub    => 'defined'
+    }
+);
+
+sub find_pub_id {
+    my ( $self, $dbref ) = @_;
+    $dbref =~ s/^[A-Z_]{4,9}://x;
+    if ( $self->has_pub($dbref) ) {
+        $self->get_pub_id($dbref);
+    }
+    my $row = $self->schema->resultset('Pub::Pub')
+        ->search( { uniquename => $dbref }, { select => 'pub_id' } );
+    if ( $row->count > 0 ) {
+        $self->set_pub_id( $dbref, $row->first->pub_id );
+        return $row->first->pub_id;
+    }
+    else {
+        $self->logger->warn( 'Column 6 ID - ' . $dbref . ' DOES NOT exist' );
+        return undef;
+    }
+}
+
 1;
 
