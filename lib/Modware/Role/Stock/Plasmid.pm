86ffe396830b492cc1d50f89be83e3911c35390e
Yogesh Pandit <yogesh.pandit@northwestern.edu>
2013-07-16 13:57:58 -0500

    Added commands & roles for stock data export
    Role for common methods to retrieve strain data
    Added commands for exporting stock data
    Command for dumping dicty strain data
    Command to dump plasmid data
    Updated cvterm role & added commons role for stock
    Updated role to fetch cvterm name
    Added commons role for strain & plasmid

---
+++ lib/Modware/Role/Stock/Plasmid.pm
@@ -0,0 +1,10 @@
+
+use strict;
+
+package Modware::Role::Stock::Plasmid;
+
+use Moose::Role;
+use namespace::autoclean;
+with 'Modware::Role::Stock::Commons';
+
+1;
---
+++ lib/Modware/Role/Stock/Strain.pm
@@ -0,0 +1,58 @@
+
+use strict;
+
+package Modware::Role::Stock::Strain;
+
+use Moose::Role;
+use namespace::autoclean;
+with 'Modware::Role::Stock::Commons';
+
+has '_strain_invent_row' => (
+    is      => 'rw',
+    isa     => 'HashRef',
+    traits  => [qw/Hash/],
+    default => sub { {} },
+    handles => {
+        set_strain_invent_row => 'set',
+        get_strain_invent_row => 'get',
+        has_strain_invent     => 'defined'
+    }
+);
+
+=item find_strain_inventory (Str $dbs_id)
+
+=cut
+
+sub find_strain_inventory {
+    my ( $self, $dbs_id ) = @_;
+    if ( $self->has_strain_invent($dbs_id) ) {
+        return $self->get_strain_invent_row($dbs_id);
+    }
+    my $old_dbxref_id
+        = $self->schema->resultset('General::Dbxref')
+        ->search( { accession => $dbs_id },
+        { select => [qw/dbxref_id accession/] } )->first->dbxref_id;
+    my $strain_invent_rs
+        = $self->legacy_schema->resultset('StockCenterInventory')->search(
+        { 'strain.dbxref_id' => $old_dbxref_id },
+        {   join   => 'strain',
+            select => [
+                qw/me.location me.color me.no_of_vials me.obtained_as me.stored_as me.storage_date/
+            ],
+            cache => 1
+        }
+        );
+    if ( $strain_invent_rs->count > 0 ) {
+        $self->set_strain_invent_row( $dbs_id, $strain_invent_rs );
+        return $self->get_strain_invent_row($dbs_id);
+    }
+
+    #else {
+    #    $self->dual_logger->warn("Cannot find strain inventory for $dbs_id");
+    #    return 0;
+    #}
+}
+
+
+
+1;
