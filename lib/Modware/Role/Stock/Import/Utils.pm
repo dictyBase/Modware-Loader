50de8b394878387447fa8e47af52f193b8b8b2db
Yogesh Pandit <yogesh.pandit@northwestern.edu>
2013-09-19 03:53:59 -0500

    Uniquename generator & method to check loaded data
    * Uniquename generator using String::Random
    * Methods to check if stock/genotype data is loaded

--- lib/Modware/Role/Stock/Import/Utils.pm
+++ lib/Modware/Role/Stock/Import/Utils.pm
@@ -6,15 +6,18 @@ use strict;
 
 use Moose::Role;
 use namespace::autoclean;
+use String::Random;
+
+with 'Modware::Role::Stock::Import::DataStash';
 
 sub prune_stock {
-	my ($self) = @_;
+    my ($self) = @_;
     $self->schema->storage->dbh_do(
         sub {
             my ( $storage, $dbh ) = @_;
             my $sth;
             for my $table (
-                qw/stock stockprop stock_cvterm stock_pub stock_genotype genotype phenotype environment stock_relationship/
+                qw/stock stockprop stock_cvterm stock_pub stock_genotype genotype phenotype environment stock_relationship phenstatement/
                 )
             {
                 $sth = $dbh->prepare(qq{DELETE FROM $table});
@@ -25,7 +28,7 @@ sub prune_stock {
     );
 }
 
-sub _mock_publications {
+sub mock_publications {
     my ($self) = @_;
     my $dicty_phen_pub;
     $dicty_phen_pub->{title} = "Dicty Stock Center Phenotyping 2003-2008";
@@ -51,8 +54,43 @@ sub trim {
 }
 
 sub is_ontology_loaded {
-	my ($self, $ontology) = @_;
-	return $self->schema->resultset('Cv::Cv')->search({name => $ontology});
+    my ( $self, $ontology ) = @_;
+    return $self->schema->resultset('Cv::Cv')
+        ->search( { name => $ontology } );
+}
+
+sub is_stock_loaded {
+    my ($self, $stock) = @_;
+    return $self->schema->resultset('Stock::Stock')
+        ->search( { 'type.name' => $stock }, { join => 'type' } )->count;
+}
+
+sub is_genotype_loaded {
+    my ($self) = @_;
+    return $self->schema->resultset('Genetic::Genotype')
+        ->search( {}, { join => 'stock_genotypes' } )->count;
+}
+
+has '_uniquename' => (
+    is      => 'rw',
+    isa     => 'HashRef',
+    traits  => [qw/Hash/],
+    default => sub { {} },
+    handles => {
+        set_uniquename => 'set',
+        has_uniquename => 'defined',
+    }
+);
+
+sub generate_uniquename {
+    my ( $self, $prefix ) = @_;
+    my $id_generator = String::Random->new();
+    my $uniquename   = $id_generator->randregex( $prefix . "[0-9]{7}" );
+    if ( !$self->has_uniquename($uniquename) ) {
+        $self->set_uniquename( $uniquename, 1 );
+        return $uniquename;
+    }
+    $self->generate_uniquename($prefix);
 }
 
 1;
