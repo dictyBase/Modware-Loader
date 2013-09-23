efb1cb6c9b12c0d8c4fb1c1e26da1d1fb79b864c
Yogesh Pandit <yogesh.pandit@northwestern.edu>
2013-09-23 14:45:28 -0500

    Subroutine to convert aray to hash for plasmid inventory

--- lib/Modware/Import/Stock/DataTransformer.pm
+++ lib/Modware/Import/Stock/DataTransformer.pm
@@ -7,7 +7,7 @@ use Moose;
 use namespace::autoclean;
 
 sub convert_row_to_strain_inventory_hash {
-	my ($self, @array) = @_;
+    my ( $self, @array ) = @_;
     my $inventory;
     $inventory->{location}        = $array[1];
     $inventory->{color}           = $array[2];
@@ -17,7 +17,18 @@ sub convert_row_to_strain_inventory_hash {
     $inventory->{storage_date}    = $array[6];
     $inventory->{private_comment} = $array[7];
     $inventory->{public_comment}  = $array[8];
-	return $inventory;
+    return $inventory;
+}
+
+sub convert_row_to_plasmid_inventory_hash {
+    my ( $self, @array ) = @_;
+    my $inventory;
+    $inventory->{location}       = $array[1];
+    $inventory->{color}          = $array[2];
+    $inventory->{stored_as}      = $array[3];
+    $inventory->{storage_date}   = $array[4];
+    $inventory->{public_comment} = $array[5];
+    return $inventory;
 }
 
 1;
