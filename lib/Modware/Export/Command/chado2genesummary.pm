c969066385af812522e60bf54f2d9b60aa0db5f1
Yogesh Pandit <yogesh.pandit@northwestern.edu>
2013-10-09 13:54:50 -0500

    Added correction of reference links
    * Improper spacing in mediawiki to be fixed.
    * Author names to be formatted as firstname-lastname

--- lib/Modware/Export/Command/chado2genesummary.pm
+++ lib/Modware/Export/Command/chado2genesummary.pm
@@ -54,13 +54,6 @@ has 'legacy_schema' => (
     builder => '_build_legacy',
 );
 
-has wiki_converter => (
-    is  => 'ro',
-    isa => 'HTML::WikiConverter',
-    default =>
-        sub { return HTML::WikiConverter->new( dialect => 'MediaWiki' ) }
-);
-
 sub _build_legacy {
     my ($self) = @_;
     my $schema = Modware::Legacy::Schema->connect(
@@ -70,55 +63,113 @@ sub _build_legacy {
     return $schema;
 }
 
+has wiki_converter => (
+    is  => 'ro',
+    isa => 'HTML::WikiConverter',
+    default =>
+        sub { return HTML::WikiConverter->new( dialect => 'MediaWiki' ) },
+    required => 1
+);
+
+has _pmids => (
+    is      => 'rw',
+    isa     => 'HashRef',
+    traits  => [qw/Hash/],
+    handles => {
+        get_pmid => 'get',
+        has_pmid => 'defined'
+    },
+    lazy    => 1,
+    builder => '_build_pub_id_pmid'
+);
+
+sub _build_pub_id_pmid {
+    my ($self) = @_;
+    my $hash;
+
+    my $pub_rs = $self->chado->resultset('Pub::Pub')->search(
+        {},
+        {   select       => [qw/me.uniquename me.pub_id/],
+            result_class => 'DBIx::Class::ResultClass::HashRefInflator'
+        }
+    );
+    while ( my $pubref = $pub_rs->next ) {
+        $hash->{ $pubref->{pub_id} } = $pubref->{uniquename};
+    }
+
+    # Few reference_no are used from legacy Reference table
+    my $ref_rs = $self->legacy_schema->resultset('Reference')->search(
+        { 'me.ref_source' => 'PUBMED' },
+        {   select       => [qw/me.reference_no me.pubmed/],
+            result_class => 'DBIx::Class::ResultClass::HashRefInflator'
+        }
+    );
+    while ( my $refref = $ref_rs->next ) {
+        $hash->{ $refref->{reference_no} } = $refref->{pubmed}
+            if !exists $hash->{ $refref->{reference_no} };
+    }
+
+    return $hash;
+}
+
 sub execute {
     my ($self) = @_;
 
-    # my $feature_props = $self->chado->storage->dbh->selectall_arrayref(
-    #     qq{
-    # SELECT d.accession, fp.value
-    # FROM featureprop fp
-    # JOIN cvterm typ ON typ.cvterm_id = fp.type_id
-    # JOIN feature f ON f.feature_id = fp.feature_id
-    # JOIN dbxref d ON d.dbxref_id = f.dbxref_id
-    # JOIN cvterm gene ON gene.cvterm_id = f.type_id
-    # WHERE typ.name = 'paragraph_no'
-    # AND gene.name = 'gene'
-    # }
-    # );
+    $self->logger->info("Exporting gene summaries");
 
-    my $tsv = Text::CSV->new( { sep_char => "\t" } );
+    my $feature_props = $self->chado->storage->dbh->selectall_arrayref(
+        qq{
+    SELECT d.accession, fp.value
+    FROM featureprop fp
+    JOIN cvterm typ ON typ.cvterm_id = fp.type_id
+    JOIN feature f ON f.feature_id = fp.feature_id
+    JOIN dbxref d ON d.dbxref_id = f.dbxref_id
+    JOIN cvterm gene ON gene.cvterm_id = f.type_id
+    WHERE typ.name = 'paragraph_no'
+    AND gene.name = 'gene'
+    }
+    );
+    $self->logger->debug(
+        scalar @{$feature_props}
+            . " gene summaries to export, as per Sequence::Featureprops" );
 
-    my $feature_para_rs
-        = $self->chado->resultset('Sequence::Featureprop')->search(
-        { 'type.name' => 'paragraph_no', 'type_2.name' => 'gene' },
-        { join => [ 'type', { feature => [qw/type dbxref/] } ], }
-        );
+    # my $feature_para_rs
+    #     = $self->chado->resultset('Sequence::Featureprop')->search(
+    #     { 'type.name' => 'paragraph_no', 'type_2.name' => 'gene' },
+    #     {   join => [ 'type', { feature => [qw/type dbxref/] } ],
+    #         result_class => 'DBIx::Class::ResultClass::HashRefInflator'
+    #     }
+    #     );
 
-    while ( my $fp = $feature_para_rs->next ) {
+    # while ( my $fp = @{$feature_props} ) {
+    for my $fp ( @{$feature_props} ) {
         my $para_rs
             = $self->legacy_schema->resultset('Paragraph')
-            ->find( { 'paragraph_no' => $fp->value },
+            ->find( { 'paragraph_no' => $fp->[1] },
             { select => [qw/me.written_by me.paragraph_text/] } );
 
-        my $text = $self->extract_text( $para_rs->paragraph_text );
-        my @array = ( $fp->feature->dbxref->accession, $para_rs->written_by,
-            $text, "\n" );
-        $tsv->print( $self->output_handler, \@array );
-        say sprintf "%s\t%s\t%s\n", $fp->feature->dbxref->accession,
-            $para_rs->written_by, $text;
+        my $wiki
+            = $self->convert_xml_to_mediawiki( $para_rs->paragraph_text );
+        my $ddbg_id = $fp->[0];
+        my $outstr  = sprintf "%s\t%s\t%s\n",
+            $ddbg_id, $para_rs->written_by,
+            $wiki;
+        $self->output_handler->print($outstr);
     }
+    $self->output_handler->close();
+    $self->logger->info(
+        "Export completed. Output written to " . $self->output );
 }
 
-sub extract_text {
+sub convert_xml_to_mediawiki {
     my ( $self, $paragraph ) = @_;
 
     my $xml_parser = XML::Twig->new();
     my $xml        = $xml_parser->parse($paragraph);
     for my $gene ( $xml->descendants('locus') ) {
-        my $ddbg_id      = $gene->att('gene_id');
-        my $gene_symbol  = $gene->att('name');
-        my $to_replace   = $gene->sprint();
-        my $replace_with = "<a href=\"/gene/$ddbg_id\">$gene_symbol</a>";
+        my $to_replace = $gene->sprint();
+        my $replace_with
+            = "<a href=\"/gene/$gene->att('gene_id')\">$gene->att('name')</a>";
         $paragraph =~ s/$to_replace/$replace_with/;
     }
     for my $go ( $xml->descendants('go') ) {
@@ -129,7 +180,27 @@ sub extract_text {
             = "<a href=\"/ontology/go/$go_id/annotation/page/1\">$go_term</a>";
         $paragraph =~ s/$to_replace/$replace_with/;
     }
+    for my $ref ( $xml->descendants('reference') ) {
+        my $pmid;
+        if ( $ref->att('reference_no') ) {
+            $pmid = $self->get_pmid( $ref->att('reference_no') );
+        }
+        elsif ( $ref->att('pmid') ) {
+            $pmid = $ref->att('pmid');
+        }
+        if ($pmid) {
+            my $to_replace = $ref->sprint();
+            my $replace_with
+                = "<a href=\"http://www.ncbi.nlm.nih.gov/pubmed/$pmid\">$ref->text</a>";
 
+            # Regex metacharacters can exist in reference tags, thus \Q...\E
+            $paragraph =~ s/\Q$to_replace\E/$replace_with/;
+        }
+        else {
+            $self->logger->warn( "PMID does not exist for reference_no:"
+                    . $ref->att('reference_no') );
+        }
+    }
     return $self->trim( $self->wiki_converter->html2wiki($paragraph) );
 }
 
@@ -152,5 +223,10 @@ Modware::Export::Command::chado2genesummary - Export gene summary for GFF3 seque
 
 =head1 SYNOPSIS
 =head1 DESCRIPTION
+
+Command to export gene summaries & curation status notes to MediaWiki format. 
+Currently, it is in XML with custom implicit tags which are rendered as link on the front-end. 
+This command replaces such tags/links with proper 'href' for efficient conversion to MediaWiki.
+
 =head1 VERSION
 =over
