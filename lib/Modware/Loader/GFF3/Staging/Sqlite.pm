a32cb96a8ae59f667d27bbafc8392945e41055a8
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2013-10-16 17:07:07 -0500

    few update in gff3 staging loader. Added bunch of staging table and sql statements for modeling gff3 data

--- lib/Modware/Loader/GFF3/Staging/Sqlite.pm
+++ lib/Modware/Loader/GFF3/Staging/Sqlite.pm
@@ -20,9 +20,6 @@ sub create_tables {
 }
 
 sub drop_tables {
-    my ($self) = @_;
-    $self->schema->storage->dbh->do(
-        $self->sqlmanager->retr('drop_table_temp_cvtermpath') );
 }
 
 sub create_indexes {
@@ -30,54 +27,16 @@ sub create_indexes {
 
 sub bulk_load {
     my ($self) = @_;
-    $self->schema->resultset('Staging::Cvtermpath')
-        ->populate( [ $self->entries_in_cvtermpath_cache ] );
 }
 
 # Each data row is a string with four columns
 # subject type(predicate) pathdistance object
 sub add_data {
     my ( $self, $data_row ) = @_;
-    chomp $data_row;
-    my @data = split /\t/, $data_row;
-
-    my ( $subject_db_id, $subject_acc ) = $self->normalize_id( $data[0] );
-    my ( $object_db_id,  $object_acc )  = $self->normalize_id( $data[3] );
-    my ($type_acc);
-    if ( $self->has_idspace( $data[1] ) ) {
-        my @parsed = $self->parse_id( $data[1] );
-        $type_acc = $parsed[1];
-    }
-    else {
-        $type_acc = $data[1];
-    }
-
-    my $type_db_id
-        = $self->has_namespace
-        ? $self->find_or_create_dbrow( $self->namespace )->db_id
-        : $object_db_id;
-
-    my $insert_hash = {
-        pathdistance      => $data[2],
-        object_accession  => $object_acc,
-        subject_accession => $subject_acc,
-        object_db_id      => $object_db_id,
-        subject_db_id     => $subject_db_id,
-        type_accession    => $type_acc,
-        type_db_id        => $type_db_id
-    };
-    $self->add_to_cvtermpath_cache($insert_hash);
 }
 
 sub count_entries_in_staging {
-    my ($self) = @_;
-    my $counts;
-    my $schema = $self->schema;
-    for my $name ( grep {/^Staging/} $schema->sources ) {
-        $counts->{ $schema->source($name)->from }
-            = $schema->resultset($name)->count( {} );
-    }
-    return $counts;
+ 
 }
 
 with 'Modware::Loader::Role::WithStaging';
