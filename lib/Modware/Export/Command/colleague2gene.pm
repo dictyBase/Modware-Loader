33381f844c97fc2607f17efd214bc78ab3436336
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2014-10-28 13:45:11 -0500

    Added csv output.

--- lib/Modware/Export/Command/colleague2gene.pm
+++ lib/Modware/Export/Command/colleague2gene.pm
@@ -1,22 +1,20 @@
 package Modware::Export::Command::colleague2gene;
 
 use strict;
-use feature 'say';
 use Moose;
 use namespace::autoclean;
+use Text::CSV;
 extends qw/Modware::Export::Command/;
 with 'Modware::Role::Command::WithDBI';
 with 'Modware::Role::Command::WithIO';
 
-has '+input' => ( 
-    traits => [qw/NoGetopt/]
-);
+has '+input' => ( traits => [qw/NoGetopt/] );
 
 has 'statement' => (
-    is => 'ro',
-    isa => 'Str',
-    traits => [qw/NoGetopt/],
-    lazy => 1,
+    is      => 'ro',
+    isa     => 'Str',
+    traits  => [qw/NoGetopt/],
+    lazy    => 1,
     default => q{
         SELECT email.email, feature.uniquename FROM cgm_ddb.email 
         JOIN cgm_ddb.coll_email ON 
@@ -30,37 +28,41 @@ has 'statement' => (
 );
 
 has '_colleague_stack' => (
-    is => 'rw',
-    isa => 'HashRef',
-    traits => [qw/Hash/],
-    default => sub {{}},
+    is      => 'rw',
+    isa     => 'HashRef',
+    traits  => [qw/Hash/],
+    default => sub { {} },
     handles => {
-        add_colleague => 'set',
-        has_colleague => 'exists',
-        get_colleague => 'get',
+        add_colleague            => 'set',
+        has_colleague            => 'exists',
+        get_colleague            => 'get',
         get_colleagues_and_genes => 'kv',
-        prune_colleagues => 'clear'
+        prune_colleagues         => 'clear'
     }
 );
 
-
 sub execute {
     my ($self) = @_;
-    my $dbh = $self->dbh;
-    my $sth = $dbh->prepare($self->statement);
+    my $dbh    = $self->dbh;
+    my $sth    = $dbh->prepare( $self->statement );
     $sth->execute;
     my $output = $self->output_handler;
-    while(my $hashref = $sth->fetchrow_hashref('NAME_lc')) {
-        if ($self->has_colleague($hashref->{email})) {
-            my $colleague_value = $self->get_colleague($hashref->{email});
+    my $csv = Text::CSV->new( { auto_diag => 1, binary => 1 } );
+    $csv->print( $output, [ "Colleague email", "Gene IDs" ] );
+    $output->print("\n");
+    while ( my $hashref = $sth->fetchrow_hashref('NAME_lc') ) {
+        if ( $self->has_colleague( $hashref->{email} ) ) {
+            my $colleague_value = $self->get_colleague( $hashref->{email} );
             push @$colleague_value, $hashref->{uniquename};
         }
         else {
-            for my $pair($self->get_colleagues_and_genes) {
-                $output->print(sprintf("%s\t%s\n",$pair->[0],join("\t",@{$pair->[1]})));
+            for my $pair ( $self->get_colleagues_and_genes ) {
+                $csv->print($output, [$pair->[0], @{$pair->[1]}]);
+                $output->print("\n");
             }
             $self->prune_colleagues;
-            $self->add_colleague($hashref->{email},[$hashref->{uniquename}]);
+            $self->add_colleague( $hashref->{email},
+                [ $hashref->{uniquename} ] );
         }
     }
 }
