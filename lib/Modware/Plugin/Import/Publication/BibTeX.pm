e930651ce991edf104274c873a0c0d8f60a12b55
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2014-11-17 10:38:54 -0600

    A plugin to parse id, source and type of publication from bibtext files

--- lib/Modware/Plugin/Import/Publication/BibTeX.pm
+++ lib/Modware/Plugin/Import/Publication/BibTeX.pm
@@ -2,32 +2,38 @@ package Modware::Plugin::Import::Publication::BibTeX;
 use Moose::Role;
 
 sub parse_uniquename {
-    my ($self,$entry) = @_;
-    if ($entry->has('pmid')) {
+    my ( $self, $entry ) = @_;
+    if ( $entry->has('pmid') ) {
         return $entry->field('pmid');
     }
-    if ($entry->has('id')) {
-        return $entry->field('id');
+    if ( $entry->has('id') ) {
+        my $id = $entry->field('id');
+        if ( $id =~ /^PUB/ ) {
+            $id =~ s/PUB//;
+        }
+        return $id;
     }
 }
 
 sub parse_pub_source {
-    if ($entry->has('pmid')) {
+    my ($self, $entry) = @_;
+    if ( $entry->has('pmid') ) {
         return 'PubMed';
     }
     my $key = $entry->key;
-    my $id = $entry->field('id');
-    if ($key =~ /^(\w+)($id)/) {
+    my $id  = $entry->field('id');
+    if ( $key =~ /^(\w+)($id)/ ) {
+        my $source = $1;
+        return uc $source;
     }
 }
 
 sub parse_pub_type {
-    my ($self, $entry) = @_;
-    if ($entry->has('status')) {
+    my ( $self, $entry ) = @_;
+    if ( $entry->has('status') ) {
         return 'journal_article';
     }
     return 'unpublished';
 }
-
 
 1;
