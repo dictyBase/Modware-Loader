dbcd0d136ee4a1d52508e5031522f7ece789efeb
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2014-11-26 14:15:13 -0600

    Added one feature count and few location based unit tests for polypeptide features

--- t/lib/TestChadoGFF3.pm
+++ t/lib/TestChadoGFF3.pm
@@ -10,6 +10,11 @@ test 'check_feature' => sub {
         rows        => 53,
         description => 'should have 53 feature rows'
     );
+    row_ok(
+        sql => [ $self->test_sql->retr('feature_type_count'), 'polypeptide' ],
+        rows        => 4,
+        description => 'should have 4 polypeptide features'
+    );
 };
 
 test 'target_feature' => sub {
@@ -93,19 +98,21 @@ test 'feature_locations' => sub {
     my ($self)   = @_;
     my $test_sql = $self->test_sql;
     my $flocs    = [
-        [ 1000, 2000, 'trans-1' ],
-        [ 5000, 6000, 'c128.1' ],
-        [ 8000, 9000, 'c128.2' ],
-        [ 1999, 3000, 'tier0' ],
-        [ 2800, 2900, 'utr1' ],
-        [ 2500, 2551, 'parent2' ]
+        [ 1000,  2000,  'trans-1', 'Contig1' ],
+        [ 5000,  6000,  'c128.1',  'Contig1' ],
+        [ 8000,  9000,  'c128.2',  'Contig1' ],
+        [ 1999,  3000,  'tier0',   'Contig1' ],
+        [ 2800,  2900,  'utr1',    'Contig1' ],
+        [ 2500,  2551,  'parent2', 'Contig1' ],
+        [ 1050,  1440,  'poly-1',  'Contig1' ],
+        [ 32100, 34900, 'poly-8',  'Contig3' ]
     ];
 
     for my $row (@$flocs) {
         row_ok(
             sql => [
                 $test_sql->retr('feature_featureloc_rows'),
-                $row->[2], 'Contig1', $row->[0], $row->[1], 0
+                $row->[2], $row->[-1], $row->[0], $row->[1], 0
             ],
             rows => 1,
             description =>
