0b4537954706ecc28d95d5d82d807bd5581569ab
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2015-01-22 16:33:54 -0600

    Added code for finding or creating a db entry

--- lib/Modware/Create/FeatureVersion/Sqlite.pm
+++ lib/Modware/Create/FeatureVersion/Sqlite.pm
@@ -2,16 +2,22 @@ package Modware::Create::FeatureVersion::Sqlite;
 use Moose;
 
 sub create {
-    my ($self, $schema, $sqlmanager) = @_;
+    my ($self, $app, $schema, $sqlmanager) = @_;
     my $dbh = $schema->storage->dbh;
-    $dbh->do($sqlmanager->retr('insert_new_dbxref_with_version'));
+    my $dbrow = $schema->resultset('General::Db')->find_or_new({name => $app->db_name});
+    if (!$dbrow->in_storage) {
+        $dbrow->url($app->db_url) if $app->db_url;
+        $dbrow->urlprefix($app->db_urlprefix) if $app->db_urlprefix;
+        $dbrow->insert;
+    }
+    my $retval = $dbh->do($sqlmanager->retr('insert_new_dbxref_with_version'), {}, $app->db_name);
 
     my $sth = $dbh->prepare($sqlmanager->retr('select_all_dbxrefs_with_version'));
-    $sth->execute();
-    while(my ($id, $accession) = $sth->fetch) {
+    $sth->execute($dbrow->db_id);
+    while(my ($id, $accession) = $sth->fetchrow_array()) {
         $dbh->do($sqlmanager->retr('update_feature_with_dbxref_id'), {}, $id, $accession);
         my ($feature_id) = $dbh->selectrow_array($sqlmanager->retr('select_feature_id'), {}, $accession);
-        $dbh->do($sqlmanager->retr('update_feature_with_uniquename'), {}, $accession, $feature_id);
+        $dbh->do($sqlmanager->retr('update_feature_with_uniquename'), {}, $accession, $id, $feature_id);
     }
 }
 
