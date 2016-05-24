993ec53d3146255f646f5b9466ac8de043ec2268
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2016-05-24 14:31:01 -0500

    Removed underscore from keys to skip unnecessary substitution.

--- lib/Modware/Import/Stock/DataTransformer.pm
+++ lib/Modware/Import/Stock/DataTransformer.pm
@@ -11,12 +11,12 @@ sub convert_row_to_strain_inventory_hash {
     my $inventory;
     $inventory->{location}        = $array[1];
     $inventory->{color}           = $array[2];
-    $inventory->{number_of_vials} = $array[3];
-    $inventory->{obtained_as}     = $array[4];
-    $inventory->{stored_as}       = $array[5];
-    $inventory->{storage_date}    = $array[6];
-    $inventory->{private_comment} = $array[7];
-    $inventory->{public_comment}  = $array[8];
+    $inventory->{'number of vials'} = $array[3];
+    $inventory->{'obtained as'}     = $array[4];
+    $inventory->{'stored as'}       = $array[5];
+    $inventory->{'storage date'}    = $array[6];
+    $inventory->{'private comment'} = $array[7];
+    $inventory->{'public comment'}  = $array[8];
     return $inventory;
 }
 
